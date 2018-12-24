4868251dc6a08e07cd66208fc83a7da4625515c5
harshad1011 <learningkoolstuff@gmail.com>
2018-12-24 23:32:07 +0530

    added: main app component

--- backend/frontend/src/app/app.component.ts
+++ backend/frontend/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  products = [{ name: 'Product1' }];
+
+  mousehover = function (flag) {
+    console.log("mouse entered: ", flag);
+  }
 }
--- backend/frontend/src/app/app.module.ts
+++ backend/frontend/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material';
+import { ProductsComponent } from './products/products.component';
+import { SliderComponent } from './slider/slider.component';
+import { CollectionsComponent } from './collections/collections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    SliderComponent,
+    CollectionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
